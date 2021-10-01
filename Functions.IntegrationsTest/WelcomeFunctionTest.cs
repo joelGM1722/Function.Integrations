5a0715d6d48ba8eaaf1874715dc3d62fc56f35d2
joel <joel85166@gmail.com>
2021-09-30 22:32:20 -0500

    Se agrega el proyecto

---
+++ Functions.IntegrationsTest/WelcomeFunctionTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Functions.IntegrationsTest
+{
+    [Collection(nameof(FunctionTestCollections))]
+    public class WelcomeFunctionTest
+    {
+        private FunctionTestFixture testFixture;
+        private HttpResponseMessage responseMessage;
+
+        public  WelcomeFunctionTest(FunctionTestFixture fixture)
+        {
+            testFixture = fixture;
+        }
+
+        [Fact]
+        public async Task TestWhenFuctionisInvoked()
+        {
+            responseMessage = await testFixture.Client.GetAsync("api/Welcome?name=Luis+Hernandez");
+            Assert.True(responseMessage.IsSuccessStatusCode);
+        }
+
+        [Fact]
+        public async Task Post_DeleteAllMessagesHandler_ReturnsRedirectToRoot()
+        {
+            responseMessage = await testFixture.Client.GetAsync("/");
+            Assert.True(responseMessage.IsSuccessStatusCode);
+        }
+
+    }
+}
